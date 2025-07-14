238565cdcef20951f3252dd7855f2d88f5e216dc
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-14 15:22:26 +0200

    add main to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import Header from './assets/components/Header'
+import Main from './assets/components/Main'
 
 
 function App() {
   
 
   return (
+    <>
     <Header/>
+    <Main/>
+    </>
+    
+
   )
 }
 
---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main(){
+    return (
+        <main className="bg-dark">
+
+        <h1 className="container text-light py-5">-- Content goes here --</h1>
+        </main>
+    )
+}
