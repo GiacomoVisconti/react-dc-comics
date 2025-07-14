b35c9646e2f092f1c49659549fc28afb1465d4d9
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-07-14 17:14:31 +0200

    fix main s  text

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,7 +1,9 @@
 export default function Main(){
     return (
         <main className="bg-dark">
-            <h1 className="container text-light py-5 m-0">-- Content goes here --</h1>
+            <div className="container">
+            <h1 className="text-light py-5 m-0">-- Content goes here --</h1>
+            </div>
         </main>
     )
 }
